150341583f43fbbb080ac66b68e108bb92eb5d99
Tom <Tom@DESKTOP-P8F94EL>
2025-12-18 21:20:04 +0200

    implemented `SharedVector` class with thread-safe vector operations including addition, negation, dot product, and transposition, managed by read-write locks.

--- src/main/java/memory/SharedVector.java
+++ src/main/java/memory/SharedVector.java
@@ -10,54 +10,109 @@ public class SharedVector {
 
     public SharedVector(double[] vector, VectorOrientation orientation) {
         // TODO: store vector data and its orientation
+        this.vector = vector;
+        this.orientation = orientation;
     }
 
     public double get(int index) {
         // TODO: return element at index (read-locked)
-        return 0;
+        return vector[index];
     }
 
     public int length() {
         // TODO: return vector length
-        return 0;
+        return vector.length;
     }
 
     public VectorOrientation getOrientation() {
         // TODO: return vector orientation
-        return null;
+       return orientation;
     }
 
     public void writeLock() {
         // TODO: acquire write lock
+        lock.writeLock().lock();
+
     }
 
     public void writeUnlock() {
         // TODO: release write lock
+        lock.writeLock().unlock();
+
     }
 
     public void readLock() {
         // TODO: acquire read lock
+        lock.readLock().lock();
     }
 
     public void readUnlock() {
         // TODO: release read lock
+        lock.readLock().unlock();
     }
 
     public void transpose() {
         // TODO: transpose vector
+        lock.writeLock().lock();
+        try {
+            if (this.orientation == VectorOrientation.ROW_MAJOR)
+                this.orientation = VectorOrientation.COLUMN_MAJOR;
+            else
+                this.orientation = VectorOrientation.ROW_MAJOR;
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void add(SharedVector other) {
         // TODO: add two vectors
+        if (this.length() != other.length()) // checks if the vectors in the same size
+            throw new IllegalArgumentException("Illegal operation: dimensions mismatch");
+        try
+        {
+            this.writeLock();
+            other.readLock();
+            for (int i = 0; i < this.length(); i++) {
+                this.vector[i] += other.get(i);
+            }
+        }
+        finally {
+            other.lock.readLock().unlock();
+            this.lock.writeLock().unlock();
+        }
     }
 
     public void negate() {
         // TODO: negate vector
+        lock.writeLock().lock();
+        try {
+            for (int i = 0; i < this.length(); i++) {
+                vector[i] = -vector[i];
+            }
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public double dot(SharedVector other) {
         // TODO: compute dot product (row · column)
-        return 0;
+        if (this.length() != other.length()) // checks if the vectors in the same size
+            throw new IllegalArgumentException("Illegal operation: dimensions mismatch");
+        double result = 0;
+        try
+        {
+            this.readLock();
+            other.readLock();
+
+            for (int i = 0; i < this.length(); i++) {
+                result += this.vector[i] * other.get(i);
+            }
+        }
+        finally {
+            this.readUnlock();
+            other.readUnlock();
+        }
+        return result;
     }
 
     public void vecMatMul(SharedMatrix matrix) {
